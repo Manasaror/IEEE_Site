2c6225f7f61e6d4e0053eb7199f6607f8934ec7c
Manasaror <arora.manas10th@gmail.com>
2026-07-02 14:58:50 +0530

    feat: establish global design system (typography, color, spacing, radii, shadows)

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+﻿/* eslint-disable @typescript-eslint/triple-slash-reference -- Astro's standard env.d.ts pattern */
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_SITE_URL: string;
+  readonly PUBLIC_SOCIAL_INSTAGRAM: string;
+  readonly PUBLIC_SOCIAL_LINKEDIN: string;
+  readonly PUBLIC_SOCIAL_TWITTER: string;
+  readonly PUBLIC_SOCIAL_GITHUB: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
