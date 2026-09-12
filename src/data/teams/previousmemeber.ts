d5f92215a1c07144188d8a7f84428d51e09fe66f
Divyansha Kukreti <divy.ansha.kukreti@gmail.com>
2026-09-12 22:37:37 +0530

    Update previous team members

--- src/data/teams/previousmemeber.ts
+++ src/data/teams/previousmemeber.ts
@@ -5,18 +5,135 @@
 
 export interface PastMember {
   name: string;
-  branch: string;
   role?: string;
 }
 
+
+
 export const pastMembers2026: PastMember[] = [
-  { name: 'Rahul Sharma', branch: 'Computer Science & Engineering', role: 'Technical Lead' },
-  { name: 'Priya Verma', branch: 'Electronics & Communication', role: 'Executive Member' },
+  { name: 'Jiyanshi Batra ', role: 'Branch Chair'},
+  { name: 'Paras Dhiman ', role: 'Branch Vice Chair' },
+  { name: 'Vanshika Painuly', role: 'Secretary'},
+  { name:'Akriti Chauhan' , role: 'Treasurer' },
+  { name:'Muskan Singh' , role: 'Creative Media Head' },
+  { name:'Shivam Dangwal' , role: 'Web Master' }
   // 👇 Naye 2026 members yahan add karein:
 ];
 
+
 export const pastMembers2025: PastMember[] = [
-  { name: 'Amit Kumar', branch: 'Electrical Engineering', role: 'Chairperson' },
-  { name: 'Neha Gupta', branch: 'Information Technology', role: 'Vice Chairperson' },
+  { name: "Tejas Chhabra", role: "Branch Chair" },
+  { name: "Nitin Goswami", role: "Branch Chair" },
+  { name: "Jiyanshi Batra", role: "Branch Chair" },
+
+  { name: "Parth Uniyal", role: "Branch Vice Chair" },
+  { name: "Kritesh Purohit", role: "Branch Vice Chair" },
+  { name: "Rakshit Pant", role: "Branch Vice Chair" },
+
+  { name: "Vanshika Painuly", role: "Secretary" },
+  { name: "Ananya Chauhan", role: "Secretary" },
+  { name: "Asmit Rana", role: "Secretary" },
+
+  { name: "Antriksha Saklani", role: "Treasurer" },
+  { name: "Sehna Saraswat", role: "Treasurer" },
+  { name: "Akriti Chauhan", role: "Treasurer" },
+
+  { name: "Khushi Lingwal", role: "Web Master" },
+  { name: "Divyanshi Nathani", role: "Web Master" },
+  { name: "Shivam Dangwal", role: "Web Master" },
+
+  { name: "Paras Dhiman", role: "Creative Media Head" },
+  { name: "Muskan Singh", role: "Creative Media Head" }
   // 👇 Naye 2025 members yahan add karein:
 ];
+ 
+export const pastMembers2024: PastMember[] = [
+  // 2023-2024
+  { name: "Akshita Nautiyal", role: "Branch Chair" },
+  { name: "Nitin Goswami", role: "Branch Chair" },
+  { name: "Jiyanshi Batra", role: "Branch Chair" },
+
+  { name: "Kritesh Purohit", role: "Branch Vice Chair" },
+  { name: "Rakshit Pant", role: "Branch Vice Chair" },
+
+  { name: "Vanshika Painuly", role: "Secretary" },
+
+  { name: "Sehna Saraswat", role: "Treasurer" },
+
+  { name: "Khushi Lingwal", role: "Web Master" },
+  { name: "Divyanshi Nathani", role: "Web Master" },
+
+  { name: "Paras Dhiman", role: "Creative Media Head" },
+  { name: "Mansi Dhiman", role: "Creative Media Head" },
+  { name: "Muskan Singh", role: "Creative Media Head" },
+];
+
+export const pastMembers2023: PastMember[] = [
+  // 2022-2023
+  { name: "Ayush Lakhera", role: "Branch Chair" },
+  { name: "Shobhit Kanswal", role: "Branch Chair" },
+
+  { name: "Tejas Chhabra", role: "Branch Vice Chair" },
+  { name: "Shaifali Bhardwaj", role: "Branch Vice Chair" },
+
+  { name: "Parth Uniyal", role: "Secretary" },
+  { name: "Chetna Kandpal", role: "Secretary" },
+
+  { name: "Antriksha Saklani", role: "Treasurer" },
+  { name: "Simran Bisht", role: "Treasurer" },
+
+  { name: "Vivek Nathani", role: "Web Master" },
+  { name: "Tanishka Petwal", role: "Web Master" },
+  { name: "Anushka Uniyal", role: "Web Master" },
+
+  { name: "Abhishek Dixit", role: "Creative Media Head" },
+  { name: "Tanmay Bist", role: "Creative Media Head" },
+
+  { name: "Akshita Rajbhar", role: "Literary Head" },
+];
+
+export const pastMembers2022: PastMember[] = [
+  // 2021-2022
+  { name: "Raman Kukreti", role: "Branch Chair" },
+
+  { name: "Avantika Joshi", role: "Branch Vice Chair" },
+
+  { name: "Srishti Yadav", role: "Secretary" },
+
+  { name: "Ayush Lakhera", role: "Treasurer" },
+
+  { name: "Sakshi Shankar", role: "Web Master" },
+
+  { name: "Anish Singh Negi", role: "Creative Media Head" },
+];
+
+export const pastMembers2021: PastMember[] = [
+  // 2020-2021
+  { name: "Raman Kukreti", role: "Branch Chair" },
+
+  { name: "Avantika Joshi", role: "Branch Vice Chair" },
+
+  { name: "Srishti Yadav", role: "Secretary" },
+
+  { name: "Ayush Lakhera", role: "Treasurer" },
+
+  { name: "Sakshi Shankar", role: "Web Master" },
+
+  { name: "Anish Singh Negi", role: "Creative Media Head" },
+];
+
+export const pastMembers2020: PastMember[] = [
+  // 2019-2020
+  { name: "Ayush Mishra", role: "Branch Chair" },
+
+  { name: "Shreya Sati", role: "Branch Vice Chair" },
+
+  { name: "Chandra Shekhar", role: "Secretary" },
+
+  { name: "Raman Kukreti", role: "Treasurer" },
+
+  { name: "Priyanshi Negi", role: "Web Master" },
+];
+  
+
+
