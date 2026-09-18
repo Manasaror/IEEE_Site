e14d240c59d06e26fbc6b8b91e3f301f0020952c
kaushikpathakk <kaushikpathak@example.com>
2026-09-18 19:43:51 +0530

    fix: add /activities/events and /activities/robotics routes to match header nav links

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AboutPage from '@/modules/about/page';
-import ActivitiesPage from '@/modules/activities/EventsPage';
+import EventsPage from '@/modules/activities/EventsPage';
+import RoboticsPage from '@/modules/activities/RoboticsPage';
 import ContactPage from '@/modules/contact/page';
 import HomePage from '@/modules/home/page';
-import TeamsPage  from '@/modules/teams/page';
+import TeamsPage from '@/modules/teams/page';
 import { NotFoundPage } from '@/pages/NotFoundPage';
 import MainLayout from './layout/Layout';
 
@@ -16,7 +17,8 @@ export function AppRouter() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/activities" element={<ActivitiesPage />} />
+          <Route path="/activities/events" element={<EventsPage />} />
+          <Route path="/activities/robotics" element={<RoboticsPage />} />
           <Route path="/teams" element={<TeamsPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Route>
@@ -25,3 +27,4 @@ export function AppRouter() {
     </BrowserRouter>
   );
 }
+
